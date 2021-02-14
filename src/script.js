7eeae88d417f1b49027087df1d281adac94d971f
Ad Astra!!! <70359572+tusharb05@users.noreply.github.com>
2021-02-14 17:51:26 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -6,80 +6,89 @@ let measure1 = document.querySelector('.measure1');
 let measure2 = document.querySelector('.measure2');
 let measure3 = document.querySelector('.measure3');
 let measure4 = document.querySelector('.measure4');
+let head1 = document.querySelector('.head1');
+let head2 = document.querySelector('.head2');
+let head3 = document.querySelector('.head3');
+let output1 = document.querySelector('#output1');
+let output2 = document.querySelector('#output2');
+let output3 = document.querySelector('#output3');
+let unitInput = document.querySelector('#unit-input');
+let container = document.querySelector('.container');
+let valueInput = document.querySelector('.value-input');
 
 measure1.addEventListener('change', measure1OnClick);
 function measure1OnClick(e) {
 	e.preventDefault();
-	document.querySelector('#unit-input').style.display = 'none';
-	document.querySelector('.container').style.visibility = 'visible';
-	document.querySelector('.value-input').placeholder = 'Enter Kgs...';
+	unitInput.style.display = 'none';
+	container.style.visibility = 'visible';
+	valueInput.placeholder = 'Enter Kgs...';
 
-	document.querySelector('.value-input').addEventListener('input', (e) => {
+	valueInput.addEventListener('input', (e) => {
 		let kgValue = document.querySelector('.value-input').value;
 		outputArea.style.visibility = 'visible';
-		document.querySelector('.head1').innerHTML = '<h4>Grams: </h4>';
-		document.querySelector('.head2').innerHTML = '<h4>Ounces: </h4>';
-		document.querySelector('.head3').innerHTML = '<h4>Pounds: </h4>';
-		document.querySelector('#output1').innerHTML = kgValue*1000;
-		document.querySelector('#output2').innerHTML = kgValue*35.274;
-		document.querySelector('#output3').innerHTML = kgValue*2.20462;
+		head1.innerHTML = '<h4>Grams: </h4>';
+		head2.innerHTML = '<h4>Ounces: </h4>';
+		head3.innerHTML = '<h4>Pounds: </h4>';
+		output1.innerHTML = kgValue*1000;
+		output2.innerHTML = kgValue*35.274;
+		output3.innerHTML = kgValue*2.20462;
 	})
 }
 
 measure2.addEventListener('change', measure2OnClick);
 function measure2OnClick(e) {
 	e.preventDefault();
-	document.querySelector('#unit-input').style.display = 'none';
-	document.querySelector('.container').style.visibility = 'visible'
-	document.querySelector('.value-input').placeholder = 'Enter Ounces...';
+	unitInput.style.display = 'none';
+	container.style.visibility = 'visible'
+	valueInput.placeholder = 'Enter Ounces...';
 
-	document.querySelector('.value-input').addEventListener('input', (e)=>{
+	valueInput.addEventListener('input', (e)=>{
 		let ozValue = document.querySelector('.value-input').value;
 		outputArea.style.visibility = 'visible';
-		document.querySelector('.head1').innerHTML = '<h4>Grams: </h4>';
-		document.querySelector('.head2').innerHTML = '<h4>Kilograms: </h4>';
-		document.querySelector('.head3').innerHTML = '<h4>Pounds: </h4>';
-		document.querySelector('#output1').innerHTML = ozValue*28.3495;
-		document.querySelector('#output2').innerHTML = ozValue*0.0283495;
-		document.querySelector('#output3').innerHTML = ozValue*0.0625;
+		head1.innerHTML = '<h4>Grams: </h4>';
+		head2.innerHTML = '<h4>Kilograms: </h4>';
+		head3.innerHTML = '<h4>Pounds: </h4>';
+		output1.innerHTML = ozValue*28.3495;
+		output2.innerHTML = ozValue*0.0283495;
+		output3.innerHTML = ozValue*0.0625;
 	})
 }
 
 measure3.addEventListener('change', measure3OnClick);
 function measure3OnClick(e) {
 	e.preventDefault();
-	document.querySelector('#unit-input').style.display = 'none';
-	document.querySelector('.container').style.visibility = 'visible'
-	document.querySelector('.value-input').placeholder = 'Enter Pounds...';
+	unitInput.style.display = 'none';
+	container.style.visibility = 'visible'
+	valueInput.placeholder = 'Enter Pounds...';
 
-	document.querySelector('.value-input').addEventListener('input', (e)=>{
+	valueInput.addEventListener('input', (e)=>{
 		let lbsValue = document.querySelector('.value-input').value;
 		outputArea.style.visibility = 'visible';
-		document.querySelector('.head1').innerHTML = '<h4>Grams: </h4>';
-		document.querySelector('.head2').innerHTML = '<h4>Kilograms: </h4>';
-		document.querySelector('.head3').innerHTML = '<h4>Ounces: </h4>';
-		document.querySelector('#output1').innerHTML = lbsValue*453.592;
-		document.querySelector('#output2').innerHTML = lbsValue*0.453592;
-		document.querySelector('#output3').innerHTML = lbsValue*16;
+		head1.innerHTML = '<h4>Grams: </h4>';
+		head2.innerHTML = '<h4>Kilograms: </h4>';
+		head3innerHTML = '<h4>Ounces: </h4>';
+		output1.innerHTML = lbsValue*453.592;
+		output2.innerHTML = lbsValue*0.453592;
+		output3.innerHTML = lbsValue*16;
 	})
 }
 
 measure4.addEventListener('change', measure4OnCLick);
 function measure4OnCLick(e) {
 	e.preventDefault();
-	document.querySelector('#unit-input').style.display = 'none';
-	document.querySelector('.container').style.visibility = 'visible'
-	document.querySelector('.value-input').placeholder = 'Enter Grams...';
+	unitInput.style.display = 'none';
+	container.style.visibility = 'visible'
+	valueInput.placeholder = 'Enter Grams...';
 
 	document.querySelector('.value-input').addEventListener('input', (e)=>{
 		let gValue = document.querySelector('.value-input').value;
 		outputArea.style.visibility = 'visible';
-		document.querySelector('.head1').innerHTML = '<h4>Kilograms: </h4>';
-		document.querySelector('.head2').innerHTML = '<h4>Pounds: </h4>';
-		document.querySelector('.head3').innerHTML = '<h4>Ounces: </h4>';
-		document.querySelector('#output1').innerHTML = gValue/1000;
-		document.querySelector('#output2').innerHTML = gValue*0.00220462;
-		document.querySelector('#output3').innerHTML = gValue*0.035274;
+		head1.innerHTML = '<h4>Kilograms: </h4>';
+		head2.innerHTML = '<h4>Pounds: </h4>';
+		head3.innerHTML = '<h4>Ounces: </h4>';
+		output1.innerHTML = gValue/1000;
+		output2.innerHTML = gValue*0.00220462;
+		output3.innerHTML = gValue*0.035274;
 	})
 }
 
